8d00fad1250579b57687ac46dd74612839018851
rampage0131 <jake.docker0131@gmail.com>
2015-04-12 02:55:42 +0700

    Add Dragon Evaluator

---
+++ Assets/Scripts/Evaluator/Dragon.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+// 13 card straight in order
+public class Dragon : IEvaluator<Dragon> {
+	CardSet dragon = new CardSet();
+
+	protected override void PreEvaluate () {
+		dragon.Add (cardSet [0]);
+	}
+
+	public override void Evaluate(int index) {
+		if (dragon[dragon.Count - 1].Score + 1 == cardSet[index].Score) {
+			dragon.Add (cardSet [index]);
+		}
+	}
+
+	protected override void PostEvaluate () {
+		if (dragon.Count == 13)
+			results.Add (dragon);
+	}
+}
