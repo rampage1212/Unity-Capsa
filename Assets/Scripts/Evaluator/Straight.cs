dec11c5af06c398f793d966d4bcb82b31888d42a
rampage0131 <jake.docker0131@gmail.com>
2015-04-12 11:56:35 +0700

    Move Straight Evaluation to Static function

--- Assets/Scripts/Evaluator/Straight.cs
+++ Assets/Scripts/Evaluator/Straight.cs
@@ -1,47 +1,61 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Straight : IEvaluator<Straight> {
 	CardSet bucket = new CardSet();
 	CardSet special = new CardSet(); // A-2-3-4-5 or 2-3-4-5-6 Straight
 
 	protected override void PreEvaluate () {
-		bucket.Clear ();
-		bucket.Add (cardSet [0]);
-
-		special.Clear ();
-		if (cardSet [0].Nominal == "3")
-			special.Add (cardSet [0]);
+		PreEvaluateStraight (cardSet[0], ref bucket, ref special);
 	}
 
 	public override void Evaluate(int index) {
-		int step = cardSet [index].Score - bucket [bucket.Count - 1].Score;
+		EvaluateStraight (ref results, cardSet [index], ref bucket, ref special, filter);
+	}
 
+	protected override void PostEvaluate () {
+		PostEvaluateStraight (ref results, ref bucket, ref special);
+	}
+
+	public static void PreEvaluateStraight(Card firtCard, ref CardSet bucket, ref CardSet special) {
+		bucket.Clear ();
+		bucket.Add (firtCard);
+		
+		special.Clear ();
+		if (firtCard.Nominal == "3")
+			special.Add (firtCard);
+	}
+
+	public static void EvaluateStraight(ref List<CardSet> results, Card card, ref CardSet bucket, ref CardSet special, Func<Card, bool> filter) {
+		int step = card.Score - bucket [bucket.Count - 1].Score;
+		
 		if (step == 1)
-			bucket.Add (cardSet [index]);
+			bucket.Add (card);
 		else if (step > 1)
 			bucket.Clear();
-
+		
 		// If 5 combination already found, add to result and remove first item for next possibility
 		if (bucket.Count == 5) {
 			if (filter(bucket[4]))
 				results.Add(bucket);
 			bucket.RemoveAt(0);
 		}
-
+		
 		// Special Straight evaluation
 		if (special.Count == 0)
 			return;
-		var curr_n = cardSet [index].Nominal;
+		var curr_n = card.Nominal;
 		var prev_n = special [special.Count - 1].Nominal;
 		if (prev_n == "3" && curr_n == "4" 
 		    || prev_n == "4" && curr_n == "5" 
 		    || prev_n == "5" && curr_n == "6"
 		    || ((prev_n == "5" || prev_n == "6")
 		    && (curr_n == "A" || curr_n == "2")))
-			special.Add(cardSet[index]);
+			special.Add(card);
 	}
 
-	protected override void PostEvaluate () {
+	public static void PostEvaluateStraight(ref List<CardSet> results, ref CardSet bucket, ref CardSet special) {
 		if (special.Count >= 5 && special [special.Count - 1].Nominal == "2") {
 			if (special.Count == 5){
 				results.Add(special);
@@ -50,7 +64,7 @@ public class Straight : IEvaluator<Straight> {
 				CardSet set = special.GetRange(0, 4) as CardSet;
 				set.Add(special[special.Count - 1]);
 				results.Add(set);
-
+				
 				// A-2-3-4-5
 				set = special.GetRange(0, 3) as CardSet;
 				set.Add(special[special.Count - 2]);
