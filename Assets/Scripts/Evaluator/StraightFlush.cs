004b8106b08d329a4cee1c96008a167c027c7096
rampage0131 <jake.docker0131@gmail.com>
2015-04-12 12:30:44 +0700

    Add StraightFlush Evaluator

---
+++ Assets/Scripts/Evaluator/StraightFlush.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+
+public class StraightFlush : IEvaluator<StraightFlush> {
+	CardSet bucketSpades = new CardSet();
+	CardSet specialSpades = new CardSet();
+
+	CardSet bucketHearts = new CardSet();
+	CardSet specialHearts = new CardSet();
+
+	CardSet bucketClubs = new CardSet();
+	CardSet specialClubs = new CardSet();
+
+	CardSet bucketDiamonds = new CardSet();
+	CardSet specialDiamonds = new CardSet();
+
+	protected override void PreEvaluate () {
+		Straight.PreEvaluateStraight (ref bucketSpades, ref specialSpades);
+		Straight.PreEvaluateStraight (ref bucketHearts, ref specialHearts);
+		Straight.PreEvaluateStraight (ref bucketClubs, ref specialClubs);
+		Straight.PreEvaluateStraight (ref bucketDiamonds, ref specialDiamonds);
+	}
+	
+	public override void Evaluate(int index) {
+		switch (cardSet [index].suit) {
+		case Card.Suit.Spade:
+			Straight.EvaluateStraight(ref results, cardSet[index], ref bucketSpades, ref specialSpades, filter);
+			break;
+		case Card.Suit.Heart:
+			Straight.EvaluateStraight(ref results, cardSet[index], ref bucketHearts, ref specialHearts, filter);
+			break;
+		case Card.Suit.Club:
+			Straight.EvaluateStraight(ref results, cardSet[index], ref bucketClubs, ref specialClubs, filter);
+			break;
+		case Card.Suit.Diamond:
+			Straight.EvaluateStraight(ref results, cardSet[index], ref bucketDiamonds, ref specialDiamonds, filter);
+			break;
+		}
+	}
+
+	protected override void PostEvaluate () {
+		Straight.PostEvaluateStraight (ref results, ref bucketSpades, ref specialSpades);
+		Straight.PostEvaluateStraight (ref results, ref bucketHearts, ref specialHearts);
+		Straight.PostEvaluateStraight (ref results, ref bucketClubs, ref specialClubs);
+		Straight.PostEvaluateStraight (ref results, ref specialDiamonds, ref specialDiamonds);
+
+		results.Sort ((set1, set2) => set1 [4].CompareTo (set2 [4]));
+	}
+}
